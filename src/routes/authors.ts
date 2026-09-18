b037cc7d8fab24ef42aa00e5292d81ab2e145551
qhathizwe <sphoswa2@gmail.com>
2026-09-18 11:04:45 +0200

    authors types, hardcoding authors and error handling

---
+++ src/routes/authors.ts
@@ -0,0 +1,37 @@
+import {Router, type Request, type Response} from 'express'
+import {body, param, validationResult} from 'express-validator'
+
+import {type Author } from '../types.js'
+
+const router = Router()
+
+let authors: Author[] =[
+    {id: 1, name: 'Williams Shakespear', email: 'williamS@email.com'},
+    {id: 2, name: 'Mfundi Vundla', email: 'mfundiV@gmail.com'}
+];
+
+router.get("/", (req: Request, res: Response) =>{
+    res.status(200).json(authors);
+});
+
+router.get(
+    "/:id",
+    [param("id").isInt().withMessage("ID must be an Integer.")],
+    (req: Request, res:Response) => {
+        const errors = validationResult(req);
+        console.log(errors, "errors from express-validator middleware");
+
+        if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+
+        const {id} = req.params
+        const author = authors.find((author) => author.id === parseInt(id as string));
+
+        if (!author){
+            return res.status(404).send("User not found!.")
+        }
+        res.status(200).json(author);
+    }
+)
+export default router
