a7bb2f653dec37250e620b852941c841f8e7941c
qhathizwe <sphoswa2@gmail.com>
2026-09-18 11:09:51 +0200

    adding middleware, router, express and bodyParser to the server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
+import bodyParser from "body-parser";
 import express, {type Express} from "express"
+import router from "./routes/authors.js";
+import { loggerMiddleware } from "./middleware/logger.js";
 
 const app: Express = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(loggerMiddleware);
+app.use("/v1/authors", router)
+
+
 app.listen(PORT, ()=>{
-    console.log(`Server id running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
