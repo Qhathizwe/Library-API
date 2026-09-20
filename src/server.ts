67c186923481efbac1026ce3a9a03d92e80eca3c
qhathizwe <sphoswa2@gmail.com>
2026-09-20 20:30:44 +0200

    editing authors router and adding books router to the server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import bodyParser from "body-parser";
 import express, {type Express} from "express"
-import router from "./routes/authors.js";
+
+import authorRouter from "./routes/authors.js";
+import bookRouter from "./routes/books.js"
+
 import { loggerMiddleware } from "./middleware/logger.js";
 
 const app: Express = express()
@@ -10,7 +13,8 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(loggerMiddleware);
-app.use("/v1/authors", router)
+app.use("/v1/authors", authorRouter)
+app.use("/v1/books", bookRouter)
 
 
 app.listen(PORT, ()=>{
