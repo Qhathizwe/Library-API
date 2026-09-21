48e14f9f5f6da457e23e1d61e5bf95baf316bc32
qhathizwe <sphoswa2@gmail.com>
2026-09-21 06:50:44 +0200

    filter authors books by Id function

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -2,22 +2,33 @@ import { type Response, type Request, } from "express";
 import { param, validationResult } from 'express-validator'
 import { type Author } from '../types.js'
 
-let authors: Author[] = [];
+import {books} from '../controllers/books.js'
+
+export let authors: Author[] = [];
 
 export const getAllAuthors =
     (req: Request, res: Response) => {
         res.status(200).json(authors);
     };
 
+
+//Get Author + Their Books by ID
 export const getAuthorById = 
      (req: Request, res:Response) =>{
     const { id } = req.params
     const author = authors.find((author) => author.id === parseInt(id as string));
 
     if(!author) {
-        return res.status(404).send("User not found!.")
+        return res.status(404).send("Auther not found!.")
     }
-    res.status(200).json(author);
+    // Filter books matching this author's ID
+    const authorBooks = books.filter((book) => book.authorId === author.id);
+
+    // Return combined data
+    res.status(200).json({
+        ...author,
+        books: authorBooks
+    });   
 }
 
 export const createNewAuthor = 
@@ -26,5 +37,5 @@ export const createNewAuthor =
     const newAuthor = {id: authors.length + 1, name, email}
 
     authors.push(newAuthor)
-    res.status(200).json(newAuthor)
+    res.status(201).json(newAuthor)
 }
