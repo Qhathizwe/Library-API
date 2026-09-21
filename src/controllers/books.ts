3bcdec3a802bbad8e60a10c49945019acf19b75c
qhathizwe <sphoswa2@gmail.com>
2026-09-21 05:26:50 +0200

    creating functions for books

---
+++ src/controllers/books.ts
@@ -0,0 +1,31 @@
+import { Router, type Request, type Response } from 'express'
+import { body, param, validationResult } from 'express-validator'
+
+import { type Book } from '../types.js'
+
+let books: Book[] = [];
+
+export const getAllBooks =
+    (req: Request, res: Response) => {
+        res.status(200).json(books);
+    };
+
+export const getBookById =
+    (req: Request, res: Response) => {
+        const { id } = req.params
+        const book = books.find((book) => book.id === parseInt(id as string));
+
+        if (!book) {
+            return res.status(400).send("User not found!")
+        }
+        res.status(200).json(book);
+    }
+
+export const createNewBook =
+    (req: Request, res: Response) => {
+        const { authorId, title, genre, year } = req.body;
+        const newBook = { id: books.length + 1, authorId, title, genre, year }
+
+        books.push(newBook);
+        res.status(200).json(newBook)
+    }
