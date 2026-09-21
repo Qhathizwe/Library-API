a910ee622d85a4d732289550c2a77833636debaa
qhathizwe <sphoswa2@gmail.com>
2026-09-21 09:34:56 +0200

    not found page and its content

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -2,7 +2,7 @@ import { type Response, type Request, } from "express";
 import { param, validationResult } from 'express-validator'
 import { type Author } from '../types.js'
 
-import {books} from '../controllers/books.js'
+import { books } from '../controllers/books.js'
 
 export let authors: Author[] = [];
 
@@ -11,31 +11,38 @@ export const getAllAuthors =
         res.status(200).json(authors);
     };
 
+export const getAuthorById =
+    (req: Request, res: Response) => {
+        const { id } = req.params
+        const author = authors.find((author) => author.id === parseInt(id as string));
 
-//Get Author + Their Books by ID
-export const getAuthorById = 
-     (req: Request, res:Response) =>{
-    const { id } = req.params
-    const author = authors.find((author) => author.id === parseInt(id as string));
-
-    if(!author) {
-        return res.status(404).send("Auther not found!.")
+        if (!author) {
+            return res.status(404).send("Auther not found!.")
+        }
+        res.status(200).json(author);
     }
-    // Filter books matching this author's ID
-    const authorBooks = books.filter((book) => book.authorId === author.id);
 
-    // Return combined data
-    res.status(200).json({
+export const getBooksByAutherId =
+    (req: Request, res: Response) => {
+        const { id } = req.params
+        const author = authors.find((author) => author.id === parseInt(id as string));
+
+        const authorBooks = books.filter((book) => book.authorId === author?.id);
+        if (!authorBooks) {
+            return res.status(404).send("Auther's Books are not found!.")
+        }
+          res.status(200).json({
         ...author,
         books: authorBooks
-    });   
-}
+    });
+    }
 
-export const createNewAuthor = 
-(req: Request, res: Response) => {
-    const {name, email} = req.body;
-    const newAuthor = {id: authors.length + 1, name, email}
 
-    authors.push(newAuthor)
-    res.status(201).json(newAuthor)
-}
+export const createNewAuthor =
+    (req: Request, res: Response) => {
+        const { name, email } = req.body;
+        const newAuthor = { id: authors.length + 1, name, email }
+
+        authors.push(newAuthor)
+        res.status(200).json(newAuthor)
+    }
