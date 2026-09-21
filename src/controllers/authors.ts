365be8ad118101cac0bc9daf539848dba671dbf7
qhathizwe <sphoswa2@gmail.com>
2026-09-21 14:01:19 +0200

    update author by id and delete author by id functions

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -1,5 +1,5 @@
 import { type Response, type Request, } from "express";
-import { param, validationResult } from 'express-validator'
+import { body, param, validationResult } from 'express-validator'
 import { type Author } from '../types.js'
 
 import { books } from '../controllers/books.js'
@@ -46,3 +46,35 @@ export const createNewAuthor =
         authors.push(newAuthor)
         res.status(200).json(newAuthor)
     }
+
+export const updateAutherById = 
+(req: Request, res: Response)=>{
+    const {id} = req.params;
+    const {name, email} = req.body;
+
+    const author = authors.find((a) => a.id === parseInt(id as string)) ;
+
+    if(!author) {
+        return res.status(404).json({message: "The author is not Found"})
+    }
+     author.name = name?? author.name;
+     author.email = email ?? author.email;
+
+     res.status(200).json({ message: "Author updated successfully", author });
+
+}
+
+export const deleteAutherById = 
+(req: Request, res: Response ) => {
+    const {id} = req.params
+
+    const index = authors.findIndex((i) => i.id === parseInt(id as string))
+
+    if (index === -1 ){
+        return res.status(400).json({message: `author ${id} not found`})
+    }
+    authors.splice(index, 1)
+
+    res.status(200).json({message: "Author deleted successfully"})
+
+}
